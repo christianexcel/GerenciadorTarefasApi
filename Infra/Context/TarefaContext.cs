42199b1cd504143c226974f2cc50688426ca0ae8
Christian Rafael <ti03@itb.ind.br>
2025-10-23 09:23:09 -0300

    Implementações Tarefas

--- Infra/Context/TarefaContext.cs
+++ Infra/Context/TarefaContext.cs
@@ -38,7 +38,7 @@ public class TarefaContext : DbContext
             entity.Property(u => u.Nome).HasColumnName("nome_usuario").HasMaxLength(100);
             entity.Property(u => u.Email).HasColumnName("email_usuario").HasMaxLength(150);
             entity.HasIndex(e => e.Email)
-              .IsUnique();
+                .IsUnique();
         });
 
         /*modelBuilder.Entity<Usuario>()
