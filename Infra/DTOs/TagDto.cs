dc8efefff8b329c1fe5b6cc8e2bc582ab51e6293
Christian Rafael <christianparadix@gmail.com>
2025-10-28 02:48:56 -0300

    feat: Ajuste tag

--- Infra/DTOs/TagDto.cs
+++ Infra/DTOs/TagDto.cs
@@ -7,6 +7,6 @@ public class TagDto
 {
     public int Id { get; set; }
     public string Nome { get; set; }
-    public ICollection<TarefaTagDto> TagTarefas { get; set; } = new List<TarefaTagDto>();
+    //public ICollection<TarefaTagDto> TagTarefas { get; set; } = new List<TarefaTagDto>();
 
 }
